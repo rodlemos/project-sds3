d9998544bd61a126435f6fd07b74564a2a680e2e
Rodrigo Lemos <rodrigosllemos@gmail.com>
2021-05-03 15:13:56 -0300

    Project Created

---
+++ backend/src/main/java/com/javadash/jvendas/JvendasApplication.java
@@ -0,0 +1,13 @@
+package com.javadash.jvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/javadash/jvendas/JvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadash.jvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
