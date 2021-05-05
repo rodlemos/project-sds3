e21ab555fd676e14a49b1677b644462abf650a70
Rodrigo Lemos <rodrigosllemos@gmail.com>
2021-05-05 18:05:10 -0300

    Pagination

---
+++ backend/src/main/java/com/javadash/jvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.javadash.jvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javadash.jvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
